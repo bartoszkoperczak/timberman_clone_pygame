a1cc9e1be75bbeed16ca77b4c471e06342f7381b
bartoszkoperczak <koperczakbartosz@gmail.com>
2025-05-28 20:50:01 +0200

    Maked changes in ui_manager.py, button class, game_window.py to launch ui_manager, added __init__.py

--- src/game_window.py
+++ src/game_window.py
@@ -1,6 +1,7 @@
 import pygame
 
 from src.event_manager import event_manager
+from src.ui_manager import UIManager
 
 
 class GameWindow:
@@ -12,6 +13,7 @@ class GameWindow:
         pygame.display.set_caption("TimbermanClone")
         self.running = True
         self.clock = pygame.time.Clock()
+        self.ui_manager = UIManager(self)
 
     def run(self):
         while self.running:
@@ -21,12 +23,29 @@ class GameWindow:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                self.ui_manager.handle_event(event)
 
-            self.screen.fill((0, 0, 0))
+            self.ui_manager.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(60)
 
         pygame.quit()
+    
+    def start_singleplayer(self):
+        print("Uruchamiam tryb singleplayer")
+        # Tutaj dodasz później logikę gry
+
+    def start_1vs1(self):
+        print("Uruchamiam tryb 1vs1")
+        # Tutaj dodasz później logikę gry
+
+    def start_1vsbot(self):
+        print("Uruchamiam tryb 1vsbot")
+        # Tutaj dodasz później logikę gry
+
+    def quit_game(self):  # Jeśli jeszcze tego nie masz
+        pygame.quit()
+        exit()
 
 
 
--- src/ui_manager.py
+++ src/ui_manager.py
@@ -1,5 +1,5 @@
 import pygame
-from button import Button
+from src.button import Button
 
 class UIManager:
     def __init__(self, game_window):
@@ -22,25 +22,25 @@ class UIManager:
 
     def create_main_menu(self):
         self.buttons = [
-            Button(100, 100, self.assets['singleplayer'], self.game_window.start_singleplayer),
-            Button(100, 180, self.assets['multiplayer'], self.show_multiplayer_menu),
-            Button(100, 260, self.assets['settings'], self.show_settings),
-            Button(100, 340, self.assets['quit'], self.quit_game)
+            Button(600, 200, self.assets['singleplayer'], self.game_window.start_singleplayer),
+            Button(600, 280, self.assets['multiplayer'], self.show_multiplayer_menu),
+            Button(600, 360, self.assets['settings'], self.show_settings),
+            Button(600, 440, self.assets['quit'], self.game_window.quit_game)
         ]
         self.current_screen = 'main'
 
     def show_multiplayer_menu(self):
         self.buttons = [
-            Button(100, 100, self.assets['1vs1'], self.game_window.start_1vs1),
-            Button(100, 180, self.assets['1vsbot'], self.game_window.start_1vsbot),
-            Button(100, 260, self.assets['return'], self.create_main_menu),
+            Button(600, 200, self.assets['1vs1'], self.game_window.start_1vs1),
+            Button(600, 280, self.assets['1vsbot'], self.game_window.start_1vsbot),
+            Button(600, 360, self.assets['return'], self.create_main_menu),
         ]
         self.current_screen = 'multiplayer'
 
     def show_settings(self):
         # Placeholder - add volume control or other settings later
         self.buttons = [
-            Button(100, 100, self.assets['return'], self.create_main_menu),
+            Button(600, 200, self.assets['return'], self.create_main_menu),
         ]
         self.current_screen = 'settings'
 
